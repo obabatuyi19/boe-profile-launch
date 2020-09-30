772c7efe2d0df5d1432e27c23c55adbcd4712265
Olushola <obabatuyi@gmail.com>
2020-09-30 14:25:31 -0500

    cleaned up  python code

--- build.py
+++ build.py
@@ -19,36 +19,23 @@ open('./docs/index.html', 'w+').write(combined_html)
 
 ###### end of index.html ###########
 
-# Reads top.html
-top_html = open('./templates/top.html').read()
-
 # Reads index.html
 middle_html = open('./content/about.html').read()
-
-
-# Reads bottom.html
-bottom_html = open('./templates/bottom.html').read()
 
 # combine files
 print('combining HTML')
 combined_html = top_html + middle_html + bottom_html
 print(combined_html)
 
-
 #write out
 open('./docs/about.html', 'w+').write(combined_html)
 
 ##### end of about.html #########
 
-# Reads top.html
-top_html = open('./templates/top.html').read()
 
 # Reads index.html
 middle_html = open('./content/resume.html').read()
 
-
-# Reads bottom.html
-bottom_html = open('./templates/bottom.html').read()
 
 # combine files
 print('combining HTML')
@@ -61,15 +48,10 @@ open('./docs/resume.html', 'w+').write(combined_html)
 
 ##### end of resume.html #####
 
-# Reads top.html
-top_html = open('./templates/top.html').read()
 
 # Reads index.html
 middle_html = open('./content/portfolio.html').read()
 
-
-# Reads bottom.html
-bottom_html = open('./templates/bottom.html').read()
 
 # combine files
 print('combining HTML')
@@ -82,15 +64,10 @@ open('./docs/portfolio.html', 'w+').write(combined_html)
 
 ##### end of portfolio.html ######
 
-# Reads top.html
-top_html = open('./templates/top.html').read()
-
 # Reads index.html
 middle_html = open('./content/contact.html').read()
 
 
-# Reads bottom.html
-bottom_html = open('./templates/bottom.html').read()
 
 # combine files
 print('combining HTML')
