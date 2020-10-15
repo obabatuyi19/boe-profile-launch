7b0a8cc83a271550bbc4e211b9cca99d4fdf5626
Olushola <obabatuyi@gmail.com>
2020-10-14 23:58:06 -0500

    clean up and added blog posts

--- build.py
+++ build.py
@@ -1,80 +1,103 @@
-# Reads top.html
-top_html = open('./templates/top.html').read()
 
-# Reads index.html
-middle_html = open('./content/index.html').read()
+def main():
+
+    pages = [
+        {   
+            "filename": "content/about.html",
+            "output": "docs/about.html",
+            "title": "About Me",
+        },
+        {
+            "filename": "content/contact.html",
+            "output": "docs/contact.html",
+            "title": "My Contact",
+        },
+        {
+            "filename": "content/index.html",
+            "output": "docs/index.html",
+            "title": "My Home page",
+        },
+        {
+            "filename": "content/portfolio.html",
+            "output": "docs/portfolio.html",
+            "title": "My Portfolio",
+        },
+        {
+            "filename": "content/resume.html",
+            "output": "docs/resume.html",
+            "title": "My Resume",
+        },
+
+    ]
+
+    blog_posts = [
+        {
+        "filename": "blog/1.html",
+        "output": "docs/blog_post_1.html",
+        "date": "September 3rd, 2018",
+        "title": "My experience so far as a noob at Kickstart Coding",
+        },
+        {
+        "filename": "blog/2.html",
+        "output": "docs/blog_post_2.html",
+        "date": "September 10th, 2018",
+        "title": "So far I really like Ubuntu Linux",
+        },
+        {
+        "filename": "blog/3.html",
+        "output": "docs/blog_post_3.html",
+        "date": "September 15th, 2018",
+        "title": "My thoughts on Python so far",
+        },
+        
+    ]
+    
+
+    for page in pages:
+        #print('page:', page)
+
+        # Read in the base template
+        template = open("./templates/base.html").read()
+        #print(template)
+
+        # Read in the content of the index HTML page
+        #print(page["filename"])
+        index_content = open(page["filename"]).read()
+        #print(index_content)
+
+        # Use the string replace
+        finished_index_page = template.replace("{{content}}", index_content).replace("{{title}}", page['title'])
+
+        open(page["output"], "w+").write(finished_index_page)
+
+        page_title = page['title']
+        print(page_title)
 
 
-# Reads bottom.html
-bottom_html = open('./templates/bottom.html').read()
+    ############ blog posts section ##########################
+    for blog in blog_posts:
+        #print('blog:', blog)
 
-# combine files
-print('combining HTML')
-combined_html = top_html + middle_html + bottom_html
-print(combined_html)
+        # Read in the blog_base template
+        template = open("./templates/blog_base.html").read()
+        #print(template)
 
+        # Read in the content of the index HTML page
+        #print(page["filename"])
+        blog_index_content = open(blog["filename"]).read()
+        #print(index_content)
 
-#write out
-open('./docs/index.html', 'w+').write(combined_html)
+        # Use the string replace
+        finished_index_blog = template.replace("{{content}}", blog_index_content).replace("{{title}}", blog['title'])
 
-###### end of index.html ###########
+        open(blog["output"], "w+").write(finished_index_blog)
 
-# Reads index.html
-middle_html = open('./content/about.html').read()
+        blog_title = blog['title']
+        print(blog_title)
 
-# combine files
-print('combining HTML')
-combined_html = top_html + middle_html + bottom_html
-print(combined_html)
-
-#write out
-open('./docs/about.html', 'w+').write(combined_html)
-
-##### end of about.html #########
+    ###### End of blog posts ############################        
+if __name__ == "__main__":
+    main()
 
 
-# Reads index.html
-middle_html = open('./content/resume.html').read()
-
-
-# combine files
-print('combining HTML')
-combined_html = top_html + middle_html + bottom_html
-print(combined_html)
-
-
-#write out
-open('./docs/resume.html', 'w+').write(combined_html)
-
-##### end of resume.html #####
-
-
-# Reads index.html
-middle_html = open('./content/portfolio.html').read()
-
-
-# combine files
-print('combining HTML')
-combined_html = top_html + middle_html + bottom_html
-print(combined_html)
-
-
-#write out
-open('./docs/portfolio.html', 'w+').write(combined_html)
-
-##### end of portfolio.html ######
-
-# Reads index.html
-middle_html = open('./content/contact.html').read()
-
-
-
-# combine files
-print('combining HTML')
-combined_html = top_html + middle_html + bottom_html
-print(combined_html)
-
-
-#write out
-open('./docs/contact.html', 'w+').write(combined_html)
 
--- build.sh
+++
@@ -1,5 +0,0 @@
-cat templates/top.html content/index.html templates/bottom.html > docs/index.html
-cat templates/top.html content/about.html templates/bottom.html > docs/about.html
-cat templates/top.html content/resume.html templates/bottom.html > docs/resume.html
-cat templates/top.html content/portfolio.html templates/bottom.html > docs/portfolio.html
-cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
